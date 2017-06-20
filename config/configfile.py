e5fce8411c33a954bbcd5914cb86550186c30406
Dario <dariomsn@hotmail.it>
2017-06-20 14:02:00 +0200

    added config folder and configfile

---
+++ config/configfile.py
@@ -0,0 +1,26 @@
+# ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
+# Copyright (C) 2017  Dario dariomsn@hotmail.it
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+# the token of the bot you get from t.me/botfather (type: str)
+bot_token = "token"
+
+# the name of the database (type: str)
+db_name = "database.db"
+
+# a python list containing the telegram ids of the admins of the bot (type: ids are int)
+admins = []
