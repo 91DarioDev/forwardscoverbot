70152b21575ad07e1867cde5e29c46cc4c756e3d
Dario <dariomsn@hotmail.it>
2017-07-08 17:38:19 +0200

    better help message

--- commands.py
+++ commands.py
@@ -22,10 +22,10 @@ import database
 
 def help_command(bot, update):
 	text = "<b>Do you want to send a message to someone or in a group, but you want to avoid that someone could \
-spread it on telegram with your name? This bot just echos your messages</b>.\nSend here what you \
+spread it on telegram with your name? This bot just echos your messages</b>.\n\nSend here what you \
 want and you will get the same message back, then forward the message where you want \
-and the forward label will have the name of this bot.\nIt works also if you edit messages or forward \
-messages. It also keeps the same text formatting style.\
+and the forward label will have the name of this bot.\n<i>It works also if you edit messages or forward \
+messages. It also keeps the same text formatting style.</i>\
 \n<a href=\"https://github.com/91DarioDev/ForwardsCoverBot\">Source code of the bot</a>"
 	update.message.reply_text(text=text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 
