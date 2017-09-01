ea515805ea968ead85bbc5d71c739e869be03331
91DarioDev <dariomsn@hotmail.it>
2017-09-01 18:25:20 +0200

    missing improt

--- messages.py
+++ messages.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from telegram import ParseMode
+from telegram.ext import DispatcherHandlerStop
 
 import database
 import keyboards
