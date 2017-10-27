54b85de5cbb5b6086d167ce4b13150b87f867ab7
91DarioDev <dariomsn@hotmail.it>
2017-10-28 01:47:08 +0200

    tabs to spaces

--- commands.py
+++ commands.py
@@ -25,40 +25,40 @@ import keyboards
 
 
 def help_command(bot, update):
-	keyboard = keyboards.github_link_kb()
-	text = "<b>Do you want to send a message to someone or in a group, but you want to avoid that someone could \
+    keyboard = keyboards.github_link_kb()
+    text = "<b>Do you want to send a message to someone or in a group, but you want to avoid that someone could \
 spread it on telegram with your name? This bot just echos your messages</b>.\n\nSend here what you \
 want and you will get the same message back, then forward the message where you want \
 and the forward label will have the name of this bot.\n<i>It works also if you edit messages or forward \
 messages. It also keeps the same text formatting style.</i>"
-	update.message.reply_text(text=text, parse_mode=ParseMode.HTML, reply_markup=keyboard)
+    update.message.reply_text(text=text, parse_mode=ParseMode.HTML, reply_markup=keyboard)
 
 
 def disable_web_page_preview(bot, update):
-	if not update.message.reply_to_message:
-		text = "This command permits to remove the web page preview from a message with a link.\n\n\
+    if not update.message.reply_to_message:
+        text = "This command permits to remove the web page preview from a message with a link.\n\n\
 Use it replying to the message the bot already echoed and you want to disable the preview with this command."
-		update.message.reply_text(text=text)
-		return
+        update.message.reply_text(text=text)
+        return
 
-	if not update.message.reply_to_message.text:
-		text = "This message does not have a web page preview"
-		update.message.reply_to_message.reply_text(text=text, quote=True)
-		return
+    if not update.message.reply_to_message.text:
+        text = "This message does not have a web page preview"
+        update.message.reply_to_message.reply_text(text=text, quote=True)
+        return
 
-	entities = update.message.reply_to_message.parse_entities([MessageEntity.URL, MessageEntity.TEXT_LINK])
-	if len(entities) == 0:
-		text = "This message does not have a web page preview"
-		update.message.reply_to_message.reply_text(text=text, quote=True)
-		return
+    entities = update.message.reply_to_message.parse_entities([MessageEntity.URL, MessageEntity.TEXT_LINK])
+    if len(entities) == 0:
+        text = "This message does not have a web page preview"
+        update.message.reply_to_message.reply_text(text=text, quote=True)
+        return
 
-	text = update.message.reply_to_message.text_html
-	update.message.reply_to_message.reply_text(text=text, disable_web_page_preview=True, parse_mode=ParseMode.HTML)
+    text = update.message.reply_to_message.text_html
+    update.message.reply_to_message.reply_text(text=text, disable_web_page_preview=True, parse_mode=ParseMode.HTML)
 
 
 
 @utils.only_admin
 def stats(bot, update):
-	update.message.reply_text(text=database.stats_text(), parse_mode=ParseMode.HTML)
+    update.message.reply_text(text=database.stats_text(), parse_mode=ParseMode.HTML)
 
 
--- database.py
+++ database.py
@@ -20,57 +20,57 @@ from config import configfile
 import utils
 
 def connect():
-	conn = sqlite3.connect("database/"+configfile.db_name)
-	c = conn.cursor()
-	return (conn, c)
+    conn = sqlite3.connect("database/"+configfile.db_name)
+    c = conn.cursor()
+    return (conn, c)
 
 
 def query_w(query, *params):
-	conn, c = connect()
-	c.execute(query, params)
-	conn.commit()
-	conn.close()
+    conn, c = connect()
+    c.execute(query, params)
+    conn.commit()
+    conn.close()
 
 
 def query_r(query, *params, one=False):
-	conn, c = connect()
-	c.execute(query, params)
-	try:
-		if not one:
-			return c.fetchall()
-		else:
-			return c.fetchone()
-	finally:
-		conn.close()
+    conn, c = connect()
+    c.execute(query, params)
+    try:
+        if not one:
+            return c.fetchall()
+        else:
+            return c.fetchone()
+    finally:
+        conn.close()
 
 
 def create_db():
-	query = "CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY, last_activity INTEGER)"
-	query_w(query)
+    query = "CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY, last_activity INTEGER)"
+    query_w(query)
 
 def add_user_db(user_id, time):
-	# try to update or ignore
-	query = "UPDATE OR IGNORE users SET last_activity = ? WHERE user_id = ?"
-	query_w(query, time, user_id)
-	# try to add or ignore
-	query = "INSERT OR IGNORE INTO users(user_id, last_activity) VALUES (?, ?)"
-	query_w(query, user_id, time)
+    # try to update or ignore
+    query = "UPDATE OR IGNORE users SET last_activity = ? WHERE user_id = ?"
+    query_w(query, time, user_id)
+    # try to add or ignore
+    query = "INSERT OR IGNORE INTO users(user_id, last_activity) VALUES (?, ?)"
+    query_w(query, user_id, time)
 
 def stats_text():
-	query = "SELECT count(user_id) FROM users"
-	total = query_r(query, one=True)[0]
+    query = "SELECT count(user_id) FROM users"
+    total = query_r(query, one=True)[0]
 
-	interval24h = time.time() - 60*60*24
-	query = "SELECT count(user_id) FROM users WHERE last_activity > ?"
-	last24h = query_r(query, interval24h, one=True)[0]
+    interval24h = time.time() - 60*60*24
+    query = "SELECT count(user_id) FROM users WHERE last_activity > ?"
+    last24h = query_r(query, interval24h, one=True)[0]
 
-	interval7d = time.time() - 60*60*24*7
-	query = "SELECT count(user_id) FROM users WHERE last_activity > ?"
-	last7d = query_r(query, interval7d, one=True)[0]
+    interval7d = time.time() - 60*60*24*7
+    query = "SELECT count(user_id) FROM users WHERE last_activity > ?"
+    last7d = query_r(query, interval7d, one=True)[0]
 
-	text = "<b>Total users:</b> {0}\n<b>Last7days:</b> {1}\n<b>Last24h:</b> {2}"
-	text = text.format(utils.n_dots(total), utils.n_dots(last7d), utils.n_dots(last24h))
-	return text
+    text = "<b>Total users:</b> {0}\n<b>Last7days:</b> {1}\n<b>Last24h:</b> {2}"
+    text = text.format(utils.n_dots(total), utils.n_dots(last7d), utils.n_dots(last24h))
+    return text
 
 # create the database
 create_db()
