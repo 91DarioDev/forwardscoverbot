a4271182fff45054ae88666071f8bc0012077942
91DarioDev <dariomsn@hotmail.it>
2017-10-28 14:02:34 +0200

    DATABASE HUGE OPTIMIZATION!!!

--- forwardscoverbot/dbwrapper.py
+++ forwardscoverbot/dbwrapper.py
@@ -30,52 +30,49 @@ def conn():
     return LOCAL.db
 
 
-def query_w(query, *params):
+def run_query(query, *params, read=False, one=False):
     connect = conn()
     cursor = connect.cursor()
-    cursor.execute(query, params)
-    cursor.connection.commit()
-    cursor.close()
-
-
-def query_r(query, *params, one=False):
-    connect = conn()
-    cursor = connect.cursor()
-    cursor.execute(query, params)
     try:
-        if not one:
-            return cursor.fetchall()
-        else:
-            return cursor.fetchone()
+        cursor.execute(query, params)
+        cursor.connection.commit()
+        if read:
+            if not one:
+                return cursor.fetchall()
+            else:
+                return cursor.fetchone()
+    except:
+        cursor.connection.rollback()
+        raise
     finally:
         cursor.close()
 
 
 def create_db():
     query = "CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY, last_activity INTEGER)"
-    query_w(query)
+    run_query(query)
 
 
 def add_user_db(user_id, int_time):
     # try to update or ignore
     query = "UPDATE OR IGNORE users SET last_activity = ? WHERE user_id = ?"
-    query_w(query, int_time, user_id)
+    run_query(query, int_time, user_id)
     # try to add or ignore
     query = "INSERT OR IGNORE INTO users(user_id, last_activity) VALUES (?, ?)"
-    query_w(query, user_id, int_time)
+    run_query(query, user_id, int_time)
 
 
 def stats_text():
     query = "SELECT count(user_id) FROM users"
-    total = query_r(query, one=True)[0]
+    total = run_query(query, read=True, one=True)[0]
 
     interval24h = time.time() - 60*60*24
     query = "SELECT count(user_id) FROM users WHERE last_activity > ?"
-    last24h = query_r(query, interval24h, one=True)[0]
+    last24h = run_query(query, interval24h, read=True, one=True)[0]
 
     interval7d = time.time() - 60*60*24*7
     query = "SELECT count(user_id) FROM users WHERE last_activity > ?"
-    last7d = query_r(query, interval7d, one=True)[0]
+    last7d = run_query(query, interval7d, read=True, one=True)[0]
 
     text = "<b>Total users:</b> {0}\n<b>Last7days:</b> {1}\n<b>Last24h:</b> {2}"
     text = text.format(utils.sep(total), utils.sep(last7d), utils.sep(last24h))
