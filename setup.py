afaeb56768d261a2cfef91d07d4bc38692ee0547
91DarioDev <dariomsn@hotmail.it>
2017-10-28 16:33:35 +0200

    package

---
+++ setup.py
@@ -0,0 +1,52 @@
+# ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
+# Copyright (C) 2017  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
+
+# ForwardsCoverBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# ForwardsCoverBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>.
+
+import setuptools
+
+
+setuptools.setup(
+
+    name = "forwardscoverbot",
+    version = "1",
+
+    license = "GPL-3",
+
+    author = "Dario 91DarioDev",
+    author_email = "dariomsn@hotmail.it",
+
+    install_requires = [
+        "python-telegram-bot",
+        "Pyyaml"
+    ],
+
+    packages = [
+        "forwardscoverbot",
+    ],
+
+    entry_points = {
+        "console_scripts": [
+            "forwardscoverbot = forwardscoverbot.__main__:main",
+        ],
+    },
+
+    include_package_data = True,
+    zip_safe = False,
+
+    classifiers = [
+        "Not on PyPI"
+    ],
+
+)
