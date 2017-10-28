f4144bde692442338ddaf4b5ab4fe290a798b3a2
91DarioDev <dariomsn@hotmail.it>
2017-10-28 03:12:04 +0200

    license udpate

--- config/configfile.example.py
+++ config/configfile.example.py
@@ -4,21 +4,20 @@ the file should be the same of this one, but replacing sample variables with tru
 """
 
 # ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
-# Copyright (C) 2017  Dario dariomsn@hotmail.it
+# Copyright (C) 2017  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
 
-# This program is free software: you can redistribute it and/or modify
+# ForwardsCoverBot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
+# ForwardsCoverBot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>.
 
 # the token of the bot you get from t.me/botfather (type: str)
 BOT_TOKEN = "token"
