9b85193ddf28c486077d14c39cb305b054b00d84
91DarioDev <dariomsn@hotmail.it>
2017-11-12 16:16:16 +0100

    ok

--- forwardscoverbot/config.py
+++ forwardscoverbot/config.py
@@ -1,18 +1,19 @@
 # ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
 # Copyright (C) 2017  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
-
+#
 # ForwardsCoverBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # ForwardsCoverBot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>
 
-# You should have received a copy of the GNU General Public License
-# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>.
 
 import yaml
 import sys
