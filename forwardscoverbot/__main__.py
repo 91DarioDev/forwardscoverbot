f11780da3d5ae703720c5486b624ab4eca1ef897
91DarioDev <dariomsn@hotmail.it>
2018-01-03 00:10:59 +0100

    album support

--- forwardscoverbot/__main__.py
+++ forwardscoverbot/__main__.py
@@ -22,6 +22,7 @@ from forwardscoverbot import config
 from forwardscoverbot import commands
 from forwardscoverbot import messages
 from forwardscoverbot import utils
+from forwardscoverbot import albums
 
 from telegram.ext import (
         Updater,
@@ -47,17 +48,22 @@ def main():
     # define the dispatcher
     dp = updater.dispatcher
 
+    # define jobs
+    j = updater.job_queue
+
     # messages
     dp.add_handler(MessageHandler(Filters.all, messages.before_processing), 0)
+    # albums
+    dp.add_handler(MessageHandler(Filters.photo | Filters.video, albums.possible_album_processing, pass_job_queue=True), 1)
     # messages
-    dp.add_handler(MessageHandler(Filters.all, messages.process_message, edited_updates=True), 1)
+    dp.add_handler(MessageHandler(Filters.all, messages.process_message, edited_updates=True), 2)
     # commands
-    dp.add_handler(CommandHandler(('start', 'help'), commands.help_command), 2)
-    dp.add_handler(CommandHandler('stats', commands.stats), 2)
-    dp.add_handler(CommandHandler('disablewebpagepreview', commands.disable_web_page_preview), 2)
-    dp.add_handler(CommandHandler('removecaption', commands.remove_caption), 2)
-    dp.add_handler(CommandHandler('addcaption', commands.add_caption), 2)
-    dp.add_handler(MessageHandler(Filters.command, utils.invalid_command), 2)
+    dp.add_handler(CommandHandler(('start', 'help'), commands.help_command), 3)
+    dp.add_handler(CommandHandler('stats', commands.stats), 3)
+    dp.add_handler(CommandHandler('disablewebpagepreview', commands.disable_web_page_preview), 3)
+    dp.add_handler(CommandHandler('removecaption', commands.remove_caption), 3)
+    dp.add_handler(CommandHandler('addcaption', commands.add_caption), 3)
+    dp.add_handler(MessageHandler(Filters.command, utils.invalid_command), 3)
 
 
     # handle errors
---
+++ forwardscoverbot/albums.py
@@ -0,0 +1,96 @@
+# ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
+# Copyright (C) 2017  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
+#
+# ForwardsCoverBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ForwardsCoverBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>
+
+from telegram import InputMedia, InputMediaPhoto, InputMediaVideo
+from telegram.ext import DispatcherHandlerStop
+from telegram import ChatAction
+
+
+ALBUM_DICT = {}
+
+
+def possible_album_processing(bot, update, job_queue):
+	if is_album(bot, update):
+		collect_album_items(bot, update, job_queue)
+		raise DispatcherHandlerStop
+	else:
+		return
+
+
+def is_album(bot, update):
+	""" 
+	return True or False if the message is part of an album
+	"""
+	if update.message.media_group_id is None:
+		return False
+	else:
+		return True
+
+
+def collect_album_items(bot, update, job_queue):
+	"""
+	if the media_group_id not a key in the dictionary yet:
+		- send sending action
+		- create a key in the dict with media_group_id
+		- add a list to the key and the first element is this message
+		- schedule a job in 1 sec
+	else:
+		- add the message to the list of that media_group_id
+	"""
+	media_group_id = update.message.media_group_id
+	to_collect = {}
+	to_collect['message_id'] = update.message.message_id
+	to_collect['file_id'] = update.message.photo[-1].file_id if update.message.photo else update.message.video.file_id
+	to_collect['caption'] = '' if update.message.caption is None else update.message.caption
+	to_collect['type'] = 'photo' if update.message.photo else 'video'
+	if media_group_id not in ALBUM_DICT:
+		bot.sendChatAction(
+			chat_id=update.message.from_user.id, 
+			action=ChatAction.UPLOAD_PHOTO if update.message.photo else ChatAction.UPLOAD_VIDEO
+		)
+		ALBUM_DICT[media_group_id] = []
+		ALBUM_DICT[media_group_id].append(to_collect)
+		# schedule the job
+		job_queue.run_once(send_album, 1, context=[update.message.from_user.id, media_group_id])
+	else:
+		ALBUM_DICT[media_group_id].append(to_collect)
+
+
+def send_album(bot, job):
+	user_id, media_group_id = job.context
+	items = ALBUM_DICT[media_group_id]
+	# delete from ALBUM_DICT
+	del ALBUM_DICT[media_group_id]
+	media = []
+	for item in items:
+		if item['type'] == 'photo':
+			media.append(
+				InputMediaPhoto(
+					media=item['file_id'],
+					caption=item['caption']
+				)
+			)
+		elif item['type'] == 'video':
+			media.append(
+				InputMediaVideo(
+					media=item['file_id'],
+					caption=item['caption']
+				)
+			)
+	bot.sendMediaGroup(
+		chat_id=user_id,
+		media=media
+	)
