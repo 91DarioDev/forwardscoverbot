0b4626011822c04c94deb390637f180edc0952d0
91DarioDev <dariomsn@hotmail.it>
2018-01-03 00:24:09 +0100

    spaces over tabs

--- forwardscoverbot/albums.py
+++ forwardscoverbot/albums.py
@@ -23,74 +23,74 @@ ALBUM_DICT = {}
 
 
 def possible_album_processing(bot, update, job_queue):
-	if is_album(bot, update):
-		collect_album_items(bot, update, job_queue)
-		raise DispatcherHandlerStop
-	else:
-		return
+    if is_album(bot, update):
+        collect_album_items(bot, update, job_queue)
+        raise DispatcherHandlerStop
+    else:
+        return
 
 
 def is_album(bot, update):
-	""" 
-	return True or False if the message is part of an album
-	"""
-	if update.message.media_group_id is None:
-		return False
-	else:
-		return True
+    """ 
+    return True or False if the message is part of an album
+    """
+    if update.message.media_group_id is None:
+        return False
+    else:
+        return True
 
 
 def collect_album_items(bot, update, job_queue):
-	"""
-	if the media_group_id not a key in the dictionary yet:
-		- send sending action
-		- create a key in the dict with media_group_id
-		- add a list to the key and the first element is this message
-		- schedule a job in 1 sec
-	else:
-		- add the message to the list of that media_group_id
-	"""
-	media_group_id = update.message.media_group_id
-	to_collect = {}
-	to_collect['message_id'] = update.message.message_id
-	to_collect['file_id'] = update.message.photo[-1].file_id if update.message.photo else update.message.video.file_id
-	to_collect['caption'] = '' if update.message.caption is None else update.message.caption
-	to_collect['type'] = 'photo' if update.message.photo else 'video'
-	if media_group_id not in ALBUM_DICT:
-		bot.sendChatAction(
-			chat_id=update.message.from_user.id, 
-			action=ChatAction.UPLOAD_PHOTO if update.message.photo else ChatAction.UPLOAD_VIDEO
-		)
-		ALBUM_DICT[media_group_id] = []
-		ALBUM_DICT[media_group_id].append(to_collect)
-		# schedule the job
-		job_queue.run_once(send_album, 1, context=[update.message.from_user.id, media_group_id])
-	else:
-		ALBUM_DICT[media_group_id].append(to_collect)
+    """
+    if the media_group_id not a key in the dictionary yet:
+        - send sending action
+        - create a key in the dict with media_group_id
+        - add a list to the key and the first element is this message
+        - schedule a job in 1 sec
+    else:
+        - add the message to the list of that media_group_id
+    """
+    media_group_id = update.message.media_group_id
+    to_collect = {}
+    to_collect['message_id'] = update.message.message_id
+    to_collect['file_id'] = update.message.photo[-1].file_id if update.message.photo else update.message.video.file_id
+    to_collect['caption'] = '' if update.message.caption is None else update.message.caption
+    to_collect['type'] = 'photo' if update.message.photo else 'video'
+    if media_group_id not in ALBUM_DICT:
+        bot.sendChatAction(
+            chat_id=update.message.from_user.id, 
+            action=ChatAction.UPLOAD_PHOTO if update.message.photo else ChatAction.UPLOAD_VIDEO
+        )
+        ALBUM_DICT[media_group_id] = []
+        ALBUM_DICT[media_group_id].append(to_collect)
+        # schedule the job
+        job_queue.run_once(send_album, 1, context=[update.message.from_user.id, media_group_id])
+    else:
+        ALBUM_DICT[media_group_id].append(to_collect)
 
 
 def send_album(bot, job):
-	user_id, media_group_id = job.context
-	items = ALBUM_DICT[media_group_id]
-	# delete from ALBUM_DICT
-	del ALBUM_DICT[media_group_id]
-	media = []
-	for item in items:
-		if item['type'] == 'photo':
-			media.append(
-				InputMediaPhoto(
-					media=item['file_id'],
-					caption=item['caption']
-				)
-			)
-		elif item['type'] == 'video':
-			media.append(
-				InputMediaVideo(
-					media=item['file_id'],
-					caption=item['caption']
-				)
-			)
-	bot.sendMediaGroup(
-		chat_id=user_id,
-		media=media
-	)
+    user_id, media_group_id = job.context
+    items = ALBUM_DICT[media_group_id]
+    # delete from ALBUM_DICT
+    del ALBUM_DICT[media_group_id]
+    media = []
+    for item in items:
+        if item['type'] == 'photo':
+            media.append(
+                InputMediaPhoto(
+                    media=item['file_id'],
+                    caption=item['caption']
+                )
+            )
+        elif item['type'] == 'video':
+            media.append(
+                InputMediaVideo(
+                    media=item['file_id'],
+                    caption=item['caption']
+                )
+            )
+    bot.sendMediaGroup(
+        chat_id=user_id,
+        media=media
+    )
