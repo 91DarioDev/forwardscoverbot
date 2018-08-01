a396b3e6986e6c6e4d38e359e1445ec9dda26f95
Hero home <super_big_hero@sina.com>
2018-08-01 18:18:11 +0800

    change the timer

--- hatsunebot/__main__.py
+++ hatsunebot/__main__.py
@@ -30,7 +30,7 @@ def error(bot, update, error):
 
 
 def main():
-    print("\nrunning...")
+    print("running...")
     # define the updater
     updater = Updater(token=config.BOT_TOKEN)
 
@@ -40,7 +40,7 @@ def main():
     # define jobs
     job = updater.job_queue
     job.run_repeating(
-        commands.callback_minute, interval=360, first=0)
+        commands.callback_minute, interval=60, first=0)
 
     # albums
     dp.add_handler(MessageHandler(custom_filters.album,
