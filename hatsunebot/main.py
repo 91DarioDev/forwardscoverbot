f71de9a601dbe88eab4c73a3514973398e7468cb
rikonaka <xxy1836@gmail.com>
2018-08-12 16:36:24 +0800

    auto push

--- hatsunebot/main.py
+++ hatsunebot/main.py
@@ -20,7 +20,7 @@ from telegram.ext import CommandHandler
 # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 #                    level=logging.INFO)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
