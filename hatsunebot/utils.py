9e7576a88d7b3562fdd0e715fd865c76313eec2d
rikonaka <xxy1836@gmail.com>
2018-08-13 18:07:31 +0800

    auto push for test in server

--- hatsunebot/utils.py
+++ hatsunebot/utils.py
@@ -4,12 +4,24 @@ from functools import wraps
 from hatsunebot import config
 
 from telegram.ext.dispatcher import run_async
+from telegram import ReplyKeyboardMarkup
 
 
 def sep(num, none_is_zero=False):
     if num is None:
         return 0 if none_is_zero is True else None
     return "{:,}".format(num)
+
+
+@run_async
+def common_help(bot, update):
+
+    custom_keyboard = [["Random"]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+
+    cid = update.message.chat.id
+    bot.send_message(chat_id=cid, text="Please use this",
+                     reply_markup=reply_markup)
 
 
 def full_list(in_list):
@@ -37,6 +49,9 @@ def only_admin(func):
     def wrapped(bot, update, *args, **kwargs):
         if update.message.from_user.id not in config.ADMINS:
             invalid_command(bot, update, *args, **kwargs)
+            return
+        else:
+            common_help(bot, update, *args, **kwargs)
             return
         return func(bot, update, *args, **kwargs)
     return wrapped
