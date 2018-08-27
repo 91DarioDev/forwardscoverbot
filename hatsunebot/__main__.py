2ba7aa8e71d614f0a78269b5e0cff332dd041861
rikonaka <xxy1836@gmail.com>
2018-08-27 15:04:20 +0800

    auto push for test in server

--- hatsunebot/__main__.py
+++ hatsunebot/__main__.py
@@ -66,7 +66,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.command, utils.invalid_command), 2)
 
     # handle errors
-    dp.add_error_handler(error
+    dp.add_error_handler(error)
 
     updater.start_polling()
     updater.idle()
