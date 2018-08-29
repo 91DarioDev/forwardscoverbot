fdc10c3ad414175f32ca6404039a0ea005925de4
rikonaka <xxy1836@gmail.com>
2018-08-30 03:55:36 +0800

    auto push for test in server

--- hatsunebot/albums.py
+++ hatsunebot/albums.py
@@ -167,7 +167,7 @@ def collect_album_items(bot, update, job_queue):
             elif result_list[0] > 1:
 
                 text = 'file_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
-                file_id_1, file_id_2, file_id_3)
+                    result_list[0], result_list[1], result_list[2])
                 update.message.reply_text(text=text, quote=True)
                 config.CHECK_FILE_ID_LIST.append(file_id_1)
                 delete_command(bot, update)
