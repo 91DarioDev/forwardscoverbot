d0379f5d4d2bd6fc1abedc70c1b47d71d18f19cf
rikonaka <xxy1836@gmail.com>
2018-09-19 09:34:43 +0800

    auto push for test in server

--- hatsunebot/messages.py
+++ hatsunebot/messages.py
@@ -117,94 +117,94 @@ def process_message(bot, update, remove_caption=False, custom_caption=None):
             # print(config.ADMINS_GROUP)
             return
         # here get the message
-        if update.edited_message:
-            message = update.edited_message
-        elif remove_caption:
-            message = update.message.reply_to_message
-        elif custom_caption is not None:
-            message = update.message.reply_to_message
-        else:
-            message = update.message
+    if update.edited_message:
+        message = update.edited_message
+    elif remove_caption:
+        message = update.message.reply_to_message
+    elif custom_caption is not None:
+        message = update.message.reply_to_message
+    else:
+        message = update.message
 
-        if custom_caption is None:
-            # caption is message title
-            caption = message.caption_html if (
-                message.caption and remove_caption is False) else None
-        else:
-            caption = custom_caption
+    if custom_caption is None:
+        # caption is message title
+        caption = message.caption_html if (
+            message.caption and remove_caption is False) else None
+    else:
+        caption = custom_caption
 
-        if message.photo:
+    if message.photo:
 
-            # FIVE_TYPE_LIST = [[MESSAGE_ID, FROM_CHAT_ID， FILE_ID_1, FILE_ID_2, FILE_ID_3], [MESSAGE_ID, FROM_CHAT_ID, FILE_ID, FILE_ID_2, FILE_ID_3]]
-            tmp_list = []
-            message_id = message.message_id
-            tmp_list.append(message_id)
-            # config.MESSAGE_ID_LIST.append(message_id)
-            from_chat_id = message.chat.id
-            tmp_list.append(from_chat_id)
-            # config.FROM_CHAT_ID_LIST.append(from_chat_id)
+        # FIVE_TYPE_LIST = [[MESSAGE_ID, FROM_CHAT_ID， FILE_ID_1, FILE_ID_2, FILE_ID_3], [MESSAGE_ID, FROM_CHAT_ID, FILE_ID, FILE_ID_2, FILE_ID_3]]
+        tmp_list = []
+        message_id = message.message_id
+        tmp_list.append(message_id)
+        # config.MESSAGE_ID_LIST.append(message_id)
+        from_chat_id = message.chat.id
+        tmp_list.append(from_chat_id)
+        # config.FROM_CHAT_ID_LIST.append(from_chat_id)
 
-            # now we get them all
-            for f in message.photo:
-                file_id = f.file_id
-                tmp_list.append(file_id)
-                # config.PHOTO_FILE_ID.append(file_id)
+        # now we get them all
+        for f in message.photo:
+            file_id = f.file_id
+            tmp_list.append(file_id)
+            # config.PHOTO_FILE_ID.append(file_id)
 
-            try:
-                tmp_list = full_list(tmp_list)
-            except Exception as e:
-                e = 'process_message() tmp_list failed: ' + str(e.args)
-                error_log.write_it(e)
-                return
-            if config.CHECK_STATUS == True:
-                # config.CHECK_LIST.append(tmp_list)
-                # mid = tmp_list[0]
-                file_id_1 = tmp_list[2]
-                file_id_2 = tmp_list[3]
-                file_id_3 = tmp_list[4]
+        try:
+            tmp_list = full_list(tmp_list)
+        except Exception as e:
+            e = 'process_message() tmp_list failed: ' + str(e.args)
+            error_log.write_it(e)
+            return
+        if config.CHECK_STATUS == True:
+            # config.CHECK_LIST.append(tmp_list)
+            # mid = tmp_list[0]
+            file_id_1 = tmp_list[2]
+            file_id_2 = tmp_list[3]
+            file_id_3 = tmp_list[4]
 
-                result_list = sql.check_sql_existed(
-                    file_id_1, file_id_2, file_id_3)
+            result_list = sql.check_sql_existed(
+                file_id_1, file_id_2, file_id_3)
 
-                '''
-                text = 'message_id:\n{0}:{1}\nfile_id_1:\n{2}:{3}\nfile_id_2:\n{4}:{5}\nfile_id_3:\n{6}:{7}\n'.format(
-                    mid, result_list[0], file_id_1, result_list[1], file_id_2, result_list[2], file_id_3, result_list[3])
-                text = 'message_id:\n{0}\nfile_id_1:\n{1}\nfile_id_2:\n{2}\nfile_id_3:\n{3}\n'.format(
-                    result_list[0], result_list[1], result_list[2], result_list[3])
-                '''
-                # text = 'file_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
-                #     result_list[0], result_list[1], result_list[2])
-                # update.message.reply_text(text=text, quote=True)
+            '''
+            text = 'message_id:\n{0}:{1}\nfile_id_1:\n{2}:{3}\nfile_id_2:\n{4}:{5}\nfile_id_3:\n{6}:{7}\n'.format(
+                mid, result_list[0], file_id_1, result_list[1], file_id_2, result_list[2], file_id_3, result_list[3])
+            text = 'message_id:\n{0}\nfile_id_1:\n{1}\nfile_id_2:\n{2}\nfile_id_3:\n{3}\n'.format(
+                result_list[0], result_list[1], result_list[2], result_list[3])
+            '''
+            # text = 'file_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
+            #     result_list[0], result_list[1], result_list[2])
+            # update.message.reply_text(text=text, quote=True)
 
-                if config.CHECK_SHOW == True:
+            if config.CHECK_SHOW == True:
 
-                    # more information
-                    text = 'Extra show:\nfile_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
-                        result_list[0], result_list[1], result_list[2])
-                    update.message.reply_text(text=text, quote=True)
+                # more information
+                text = 'Extra show:\nfile_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
+                    result_list[0], result_list[1], result_list[2])
+                update.message.reply_text(text=text, quote=True)
 
-                if result_list[0] == 0 and result_list[1] == 0 and result_list[2] == 0:
-
-                    config.SQL_LIST.append(tmp_list)
-                    text = 'This message is not include in MySQL, inserting...'
-                    update.message.reply_text(text=text, quote=True)
-
-                    # result_list = sql.check_sql_existed(
-                    #     file_id_1, file_id_2, file_id_3)
-                    # text = 'New: file_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
-                    #     result_list[0], result_list[1], result_list[2])
-                    # update.message.reply_text(text=text, quote=True)
-                elif result_list[0] > 1:
-
-                    text = 'file_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
-                        result_list[0], result_list[1], result_list[2])
-                    update.message.reply_text(text=text, quote=True)
-                    config.CHECK_FILE_ID_LIST.append(file_id_1)
-                    delete_command(bot, update)
-
-            else:
+            if result_list[0] == 0 and result_list[1] == 0 and result_list[2] == 0:
 
                 config.SQL_LIST.append(tmp_list)
-                config.FIVE_TYPE_LIST.append(tmp_list)
+                text = 'This message is not include in MySQL, inserting...'
+                update.message.reply_text(text=text, quote=True)
 
-            process_message_subdivision(bot, update, message, caption)
+                # result_list = sql.check_sql_existed(
+                #     file_id_1, file_id_2, file_id_3)
+                # text = 'New: file_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
+                #     result_list[0], result_list[1], result_list[2])
+                # update.message.reply_text(text=text, quote=True)
+            elif result_list[0] > 1:
+
+                text = 'file_id_1:\n{0}\nfile_id_2:\n{1}\nfile_id_3:\n{2}\n'.format(
+                    result_list[0], result_list[1], result_list[2])
+                update.message.reply_text(text=text, quote=True)
+                config.CHECK_FILE_ID_LIST.append(file_id_1)
+                delete_command(bot, update)
+
+        else:
+
+            config.SQL_LIST.append(tmp_list)
+            config.FIVE_TYPE_LIST.append(tmp_list)
+
+        process_message_subdivision(bot, update, message, caption)
