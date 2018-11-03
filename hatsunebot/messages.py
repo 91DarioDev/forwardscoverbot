9f6dc3f9a4877c89d2877b4cc03210c7da4d09b5
rikonaka <xxy1836@gmail.com>
2018-11-03 14:01:28 +0800

    add the reply for text message

--- hatsunebot/messages.py
+++ hatsunebot/messages.py
@@ -19,7 +19,9 @@ def process_message_subdivision(bot, update, message, caption):
 
     # then use the message
     if message.text:
-        # message.reply_text(text=message.text_html, parse_mode=ParseMode.HTML)
+        # here we add the reply same words
+        #message.reply_text(text=message.text_html, parse_mode=ParseMode.HTML)
+        message.reply_text(text=message.text, parse_mode=ParseMode.HTML)
         pass
 
     elif message.voice:
