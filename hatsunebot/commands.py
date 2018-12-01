8b71062e605190d8d455788121626b2b6d307906
rikonaka <xxy1836@gmail.com>
2018-12-02 02:44:17 +0800

    found the mysql error

--- hatsunebot/commands.py
+++ hatsunebot/commands.py
@@ -152,6 +152,7 @@ def Command_CallBackQueryMid(bot, job):
     sql.SQL_GetMaxTableCount()
     table_id = random.randint(0, config.NU_RANDOM)
     table_name = "{0}pic_{1}".format(config.SQL_FORMAT, table_id)
+    print(table_name)
     status = -1
     while status == -1:
         status = sql.SQL_GetMidLimited(table_name)
@@ -162,7 +163,6 @@ def Command_CallBackQueryMid_Fix():
 
     table_id = random.randint(0, config.NU_RANDOM)
     table_name = "{0}pic_{1}".format(config.SQL_FORMAT, table_id)
-    print(table_name)
     status = -1
     while status == -1:
         status = sql.SQL_GetMidLimited(table_name)
