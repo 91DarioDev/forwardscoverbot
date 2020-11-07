51ac58d83cc5d29867596d0cdb05ab91b67d789f
91DarioDev <dariomsn@hotmail.it>
2020-11-08 00:22:00 +0100

    disable apscheduler logging

--- forwardscoverbot/__main__.py
+++ forwardscoverbot/__main__.py
@@ -36,6 +36,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# disable apscheduler logging
+aps_logger = logging.getLogger('apscheduler')
+aps_logger.setLevel(logging.WARNING)
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
