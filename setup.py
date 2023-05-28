9f7ef2b6ca46296bd3a7d1e0a5f755d9e0e75cb7
Dario <dariomsn@hotmail.it>
2023-05-29 00:00:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     author_email="dariomsn@hotmail.it",
 
     install_requires=[
-        "python-telegram-bot==20.0",
+        "python-telegram-bot[ext]==20.3",
         "Pyyaml",
         "aiosqlite"
     ],
